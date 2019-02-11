b5e3072766cbe5bb3d2039cdf276c378f1f73f21
chewby <bastien.coadou@yahoo.fr>
2019-02-11 11:18:14 +0100

    AjoutReadMe

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 lucian2
+
+pour fonctionner en local : 
+
+faire composer require server --dev
+
+pour lancer le server en local : 
+
+php bin/console server:run
--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
