a10675f2a14a770c11650993cc1237ae0ba7e898
flavien44 <flavienangelo@hotmail.com>
2019-03-06 15:11:13 +0100

    AppVeyor Update 2.0

--- tests/Util/CalculatorTest.php
+++ tests/Util/CalculatorTest.php
@@ -10,7 +10,7 @@ class CalculatorTest extends TestCase
     public function testAdd()
     {
         $calculator = new Calculator();
-        $result = $calculator->add(14, 12);
+        $result = $calculator->add(30, 12);
 
         // assert that your calculator added the numbers correctly!
         $this->assertEquals(42, $result);
